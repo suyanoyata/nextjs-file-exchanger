92c7e915e5ef15ada03751d64638e352ea875c86
suyanoyata <teikukuheia@gmail.com>
2025-01-03 15:30:56 +0200

    refactor: remove unused component

--- components/header.tsx
+++
@@ -1,8 +0,0 @@
-"use server";
-
-import { users } from "@/features/users/db/users";
-
-export const Header = async () => {
-  const user = await users.api.getCurrentUser();
-  return <header>{JSON.stringify(user)}</header>;
-};
